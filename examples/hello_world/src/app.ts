1b44209aa7cdb7e02d88ef2c7fd46900c272f2ff
Hugo Freire <hfreire@abajar.com>
2018-02-01 17:57:54 +0100

    Fix hello world example for new version

--- examples/hello_world/src/app.ts
+++ examples/hello_world/src/app.ts
@@ -1,9 +1,35 @@
-import { Bootstrap } from 'phaser-mvc';
+import * as Phaser from 'phaser-ce';
+import { Witcase, BaseEngine } from 'phaser-mvc';
 import { StartupController } from './controllers/startup_controller';
 
 /*
  * Bootstrap game
  */
 window.onload = () => {
-  Bootstrap.run(new StartupController().welcome);
+  let witcase = Witcase.create<Phaser.Game>();
+
+  witcase.start((baseEngine: BaseEngine)=> {
+    const game = new Phaser.Game(
+      1000,
+      750,
+      Phaser.CANVAS,
+      'content',
+      {
+        preload: () => {
+          game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+          //wait until Phaser is ready to create first controller
+          witcase.defaultAction = new StartupController().welcome;
+          baseEngine.preload();
+        },
+        create: () => {
+          baseEngine.create();
+        },
+        update: baseEngine.update,
+        render: baseEngine.render
+      }
+    );
+
+    return game;
+  });
 };
